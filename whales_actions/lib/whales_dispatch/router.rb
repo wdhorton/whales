0b6307646330c89ff94fad9841503a90f95d4d6c
fpcyan <fionapwconn@gmail.com>
2015-12-08 16:03:23 -0500

    readd missing method end

--- whales_actions/lib/whales_dispatch/router.rb
+++ whales_actions/lib/whales_dispatch/router.rb
@@ -16,30 +16,33 @@ module WhalesDispatch
       instance_eval(&proc)
     end
 
-      def resources(controller_noun, scope, **action_restrictions)
-    @last_parent_route = "" if scope == :parent
-    controller_actions = DefaultActions.new(controller_noun)
-    controller_actions.parse_action_restrictions(action_restrictions)
-    build_resources(controller_noun, controller_actions)
+    def resources(controller_noun, scope, **action_restrictions)
+      @last_parent_route = "" if scope == :parent
+      controller_actions = DefaultActions.new(controller_noun)
+      controller_actions.parse_action_restrictions(action_restrictions)
+      build_resources(controller_noun, controller_actions)
 
-    if block_given?
-      @last_parent_route = controller_noun.to_s
-      yield
+      if block_given?
+        @last_parent_route = controller_noun.to_s
+        yield
+      end
     end
-  end
 
-  def build_resources(controller_noun, controller_actions)
-    controller_actions.actions.each do |action_name, action_hash|
+    def build_resources(controller_noun, controller_actions)
+      controller_actions.actions.each do |action_name, action_hash|
+
         resource = Resource.new(
           controller_noun, action_hash[:suffix], @last_parent_route
         )
+
         send action_hash[:method], resource.pattern, resource.classify, action_name
       end
-  end
+    end
 
-  [:get, :post, :put, :delete].each do |http_method|
-    define_method(http_method) do |pattern, controller_class, action_name|
-      add_route(pattern, http_method, controller_class, action_name)
+    [:get, :post, :put, :delete].each do |http_method|
+      define_method(http_method) do |pattern, controller_class, action_name|
+        add_route(pattern, http_method, controller_class, action_name)
+      end
     end
 
     def match(req)
@@ -48,7 +51,6 @@ module WhalesDispatch
 
     def run(req, res)
       route = match(req)
-
       if route
         route.run(req, res)
       else
@@ -56,5 +58,6 @@ module WhalesDispatch
         res.body = "Could not find matching route."
       end
     end
+    
   end
 end
